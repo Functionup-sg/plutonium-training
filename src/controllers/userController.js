cfb0016443ed8bb7a018c093f880de0527401509
Functionup-sg <sujatagarande@gmail.com>
2022-09-05 01:12:36 +0530

    solution

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,19 +3,26 @@ const userModel = require("../models/userModel");
 
 
 const createUser = async function (req, res) {
+  try{
   let data = req.body;
+  if (Object.keys(data)==0) return res.status(400).send({msg:"request body is empty"})
   let savedData = await userModel.create(data);
   console.log(req.newAtribute);
-  res.send({ msg: savedData });
+  res.status(200).send({ msg: savedData });
+}
+catch(err){
+  res.status(500).send({msg: "Error", error:err.message})
+}
 };
 //login function
 const loginUser = async function (req, res) {
+  try{
   let userName = req.body.emailId;
   let password = req.body.password;
-
+if (!(userName && password)) return res.status(400).send({msg: "please enter username and password"})
   let user = await userModel.findOne({ emailId: userName, password: password });
   if (!user)
-    return res.send({
+    return res.status(400).send({
       status: false,
       msg: "username or the password is not corerct",
     });
@@ -35,55 +42,79 @@ const loginUser = async function (req, res) {
     "functionup-plutonium"
   );
   res.setHeader("x-auth-token", token);
-  res.send({ status: true, token: token });
+  res.status(200).send({ status: true, token: token });
+}
+catch(err){
+  res.status(500).send({msg: "Error", error:err.message})
+}
 };
 
 //get user Data
 const getUserData = async function (req, res) {
+  try{
   let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
   if (!userDetails || userDetails.isDeleted)
-    return res.send({ status: false, msg: "No such user exists" });
-  res.send({ status: true, data: userDetails });
-  
+    return res.status(400).send({ status: false, msg: "No such user exists" });
+  res.status(200).send({ status: true, data: userDetails });
+  }
+
+  catch(err){
+    res.status(500).send({msg: "Error", error:err.message})
+  }
 };
 
 const updateUser = async function (req, res) {
+  try{
   let userId = req.params.userId;
   let user = await userModel.findById(userId);
   //Return an error if no user with the given id exists in the db
   if (!user || user.isDeleted) {
-    return res.send("No such user exists");
+    return res.status(400).send("No such user exists");
   }
 
   let userData = req.body;
   let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData,{new:true});
-  res.send({ status: "User update", data: updatedUser });
+  res.status(200).send({ status: "User update", data: updatedUser });
+  }
+  catch(err){
+    res.status(500).send({msg: "Error", error:err.message})
+  }
 };
 
 const postMessage = async function(req, res){
+  try{
   let message = req.body.message
   let user= await userModel.findById(req.params.userId)
   if(!user || user.isDeleted)
-  return res.send({status: false, msg: "No such user exists"})
+  return res.status(400).send({status: false, msg: "No such user exists"})
   let updatedPosts = user.posts
   updatedPosts.push(message)
   let updatedUser= await userModel.findOneAndUpdate({_id:user._id},{posts: updatedPosts},{upsert:true,new:true})
-  res.send({ status: true, data: updatedUser });
+  res.status(200).send({ status: true, data: updatedUser });
+  }
+  catch(err){
+    res.status(500).send({msg: "Error", error:err.message})
+  }
 }
 
  const deleteUser =async function(req, res){
+  try{
   let userId = req.params.userId;
   let user = await userModel.findById(userId);
   console.log(user)
   if (!user){
-    return res.send("No such user exists");
+    return res.status(400).send("No such user exists");
   }
   if (user.isDeleted){
-    return res.send("User data is already deleted");
+    return res.status(400).send("User data is already deleted");
   }
   let deleteFlag=await userModel.updateOne({_id:userId},{$set:{isDeleted:true}},{upsert:true})
-  res.send({msg:"user successfully deleted", data:deleteFlag})
+  res.status(200).send({msg:"user successfully deleted", data:deleteFlag})
+  }
+  catch(err){
+    res.status(500).send({msg: "Error", error:err.message})
+  }
   }
   
 
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,22 +1,34 @@
 const jwt= require("jsonwebtoken")
 
 const authenticate=async function(req, res, next){
+    try{
     let token = req.headers["x-Auth-token"];
     if (!token)token = req.headers["x-auth-token"];
     if(!token) return res.send({status:false, msg: "token must be present"});
     let decodedToken = jwt.verify(token, "functionup-plutonium");
     if(!decodedToken)
-    return res.send({ status: false, msg: "token is invalid"});
+    //status(401) token is invalid
+    return res.status(401).send({ status: false, msg: "token is invalid"});
     next()
+}
+catch(err){
+    res.status(500).send({msg: "Error", error:err.message})
+  }
 }
 
 const authorise=async function(req, res, next){
+    try{
     let token = req.headers["x-Auth-token"];
     if (!token)token = req.headers["x-auth-token"];
     let decodedToken = jwt.verify(token, "functionup-plutonium");
     if(decodedToken.userId!==req.params.userId)
-    return res.send({ status: false, msg: "You are not authorizer to do this task"});
+    //status(403) token is not
+    return res.status(403).send({ status: false, msg: "You are not authorizer to do this task"});
     next()
+}
+catch(err){
+    res.status(500).send({msg: "Error", error:err.message})
+  }
 }
 module.exports.authenticate=authenticate
 module.exports.authorise=authorise
