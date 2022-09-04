93ed7503a02369260f00840ed29c4e89a5fbead3
Functionup-sg <sujatagarande@gmail.com>
2022-09-04 21:43:02 +0530

    Auth-2 ass

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,19 +1,14 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-/*
-  Read all the comments multiple times to understand why we are doing what we are doing in login api and getUserData api
-*/
-const createUser = async function (abcd, xyz) {
-  //You can name the req, res objects anything.
-  //but the first parameter is always the request 
-  //the second parameter is always the response
-  let data = abcd.body;
-  let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
-  xyz.send({ msg: savedData });
-};
 
+const createUser = async function (req, res) {
+  let data = req.body;
+  let savedData = await userModel.create(data);
+  console.log(req.newAtribute);
+  res.send({ msg: savedData });
+};
+//login function
 const loginUser = async function (req, res) {
   let userName = req.body.emailId;
   let password = req.body.password;
@@ -34,65 +29,57 @@ const loginUser = async function (req, res) {
   let token = jwt.sign(
     {
       userId: user._id.toString(),
-      batch: "thorium",
+      batch: "plutonium",
       organisation: "FunctionUp",
     },
-    "functionup-plutonium-very-very-secret-key"
+    "functionup-plutonium"
   );
   res.setHeader("x-auth-token", token);
   res.send({ status: true, token: token });
 };
 
+//get user Data
 const getUserData = async function (req, res) {
-  let token = req.headers["x-Auth-token"];
-  if (!token) token = req.headers["x-auth-token"];
-
-  //If no token is present in the request header return error. This means the user is not logged in.
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-
-  console.log(token);
-
-  // If a token is present then decode the token with verify function
-  // verify takes two inputs:
-  // Input 1 is the token to be decoded
-  // Input 2 is the same secret with which the token was generated
-  // Check the value of the decoded token yourself
-
-  // Decoding requires the secret again. 
-  // A token can only be decoded successfully if the same secret was used to create(sign) that token.
-  // And because this token is only known to the server, it can be assumed that if a token is decoded at server then this token must have been issued by the same server in past.
-  let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
-
   let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
-  if (!userDetails)
+  if (!userDetails || userDetails.isDeleted)
     return res.send({ status: false, msg: "No such user exists" });
-
   res.send({ status: true, data: userDetails });
-  // Note: Try to see what happens if we change the secret while decoding the token
+  
 };
 
 const updateUser = async function (req, res) {
-  // Do the same steps here:
-  // Check if the token is present
-  // Check if the token present is a valid token
-  // Return a different error message in both these cases
-
   let userId = req.params.userId;
   let user = await userModel.findById(userId);
   //Return an error if no user with the given id exists in the db
-  if (!user) {
+  if (!user || user.isDeleted) {
     return res.send("No such user exists");
   }
 
   let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
-  res.send({ status: updatedUser, data: updatedUser });
+  if(Object.keys(userData).length == 0) return res.send({msg:"Body s empty nothing to update"})
+
+  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData,{new:true});
+  res.send({ status: "User update", data: updatedUser });
 };
+ const deleteUser =async function(req, res){
+  let userId = req.params.userId;
+  let user = await userModel.findById(userId);
+  console.log(user)
+  if (!user){
+    return res.send("No such user exists");
+  }
+  if (user.isDeleted){
+    return res.send("User data is already deleted");
+  }
+  let deleteFlag=await userModel.updateOne({_id:userId},{$set:{isDeleted:true}},{upsert:true})
+  res.send({msg:"user successfully deleted", data:deleteFlag})
+  }
+  
 
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deleteUser = deleteUser;
+
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://sujatagarande:0jNgxkDzTi6CFIdW@cluster0.mirnn.mongodb.net/sujatagarande", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
